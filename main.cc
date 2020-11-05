602bb916f46bb44abfc68a4340ca702580a4de58
test1292 <test1292@test1292.com>
2020-11-05 13:21:34 +0300

    test1

--- main.cc
+++ main.cc
@@ -1,12 +1,7 @@
-#include "stemmer/stemmer.h"
-
 #include <iostream>
+#include <string>
 
 int main() {
-    std::string s;
-    std::cin >> s;
-
-    std::cout << stemmer::stem_ru_string(s) << std::endl;
 
     return 0;
 }
