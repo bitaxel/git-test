bc471edfc610444b9b0bb63f9190543de407835d
test1292 <test1292@test1292.com>
2020-11-05 13:49:58 +0300

    add IsVowel function

--- stemmer/stemmer.cc
+++ stemmer/stemmer.cc
@@ -4,6 +4,19 @@ namespace stemmer {
 
 namespace {
 
+bool IsVowel(const char ch) noexcept
+{
+    return ch == 'а' or
+        ch == 'е' or
+        ch == 'ё' or
+        ch == 'и' or
+        ch == 'о' or
+        ch == 'у' or
+        ch == 'э' or
+        ch == 'ю' or
+        ch == 'я';
+}
+
 } // anonymous namespace
 
 std::string stem_ru_string(const std::string& word)
